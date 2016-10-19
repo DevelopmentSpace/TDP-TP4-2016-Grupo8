498e2d1f6ec7b951e3702b3428d304368bb8d47a
Manuel Chichi <manuchichi@hotmail.com>
2016-10-19 20:11:00 -0300

    Ej2 Completo.

---
+++ TP4/EJ2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dividendo, divisor;
+            Calculadora calc = new Calculadora();
+
+            Console.Write("Ingrese el dividendo: ");
+            int.TryParse(Console.ReadLine(), out dividendo);
+
+            Console.Write("Ingrese el divisor:");
+            int.TryParse(Console.ReadLine(), out divisor);
+
+            double res=double.MaxValue;
+            try
+            {
+                res = calc.Dividir(dividendo, divisor);
+            }
+            catch (DivisionPorCeroException e)
+            {
+                Console.WriteLine("Error: El dividendo ingresado no es valido. Ingrese un numero mayor a 0");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Resultado: "+ res);
+            Console.ReadKey();
+
+
+        }
+    }
+}
