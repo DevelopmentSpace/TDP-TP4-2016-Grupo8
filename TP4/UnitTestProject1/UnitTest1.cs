a21e52eb18e1d0eed9be154c63369c458e020c2d
Manuel Chichi <manuchichi@hotmail.com>
2016-11-11 17:54:58 -0300

    TP4 - Ej 7 pruebas

--- TP4/UnitTestProject1/UnitTest1.cs
+++ TP4/UnitTestProject1/UnitTest1.cs
@@ -21,5 +21,20 @@ namespace UnitTestProject1
             Assert.AreEqual(obtenido, esperado);
         }
 
+        [TestMethod]
+        public void EliminarEvento()
+        {
+            Calendario calendario = new Calendario("Carreras verano", new DateTime(1998, 6, 20));
+            Evento evento1 = new Evento("Carrera1", new DateTime(2020, 11, 9), new TimeSpan(4, 30, 0), FrecuenciaEvento.unico);
+
+            calendario.Agregar(evento1);
+            calendario.Eliminar(evento1);
+
+            Evento obtenido = calendario.ObtenerEventos()[0];
+            Evento esperado = null;
+
+            Assert.AreEqual(obtenido, esperado);
+        }
+
     }
 }
