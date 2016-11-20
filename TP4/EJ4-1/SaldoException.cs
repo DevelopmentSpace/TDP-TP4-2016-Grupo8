9836353598094878b7a1eca1bb4961b21e24d9fb
Manuel Chichi <manuchichi@hotmail.com>
2016-11-19 21:56:37 -0300

    Ej4-1 agregado.

---
+++ TP4/EJ4-1/SaldoException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ2
+{
+    class SaldoException : ApplicationException
+    {
+
+        public SaldoException(string pMensaje) : base(pMensaje)
+        {
+        }
+
+    }
+}
